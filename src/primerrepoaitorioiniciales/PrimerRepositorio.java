c874c9f82f4d0164989e350107d0c8dcc811013d
juanc <juanc@LAPTOP-MP4QF3NE>
2019-01-16 11:02:29 +0100

    Primer repositorio

---
+++ src/primerrepoaitorioiniciales/PrimerRepositorio.java
@@ -0,0 +1,15 @@
+package primerrepoaitorioiniciales;
+/**
+ * cambio repositorio
+ */
+import java.util.Scanner;
+
+public class PrimerRepositorio {
+
+	public static void main(String[] args) {
+		System.out.println("Primer reporsitio para git hab");
+		System.out.println("Juan Carlos Molina");
+
+	}
+
+}
