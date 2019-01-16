58effe6eb8a351d1571edb8949b204256844e6d8
Luis <Luis@DESKTOP-9BKJLUP>
2019-01-16 11:15:08 +0100

    Correción de escritura

--- src/primerrepoaitorioiniciales/PrimerRepositorio.java
+++ src/primerrepoaitorioiniciales/PrimerRepositorio.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class PrimerRepositorio {
 
 	public static void main(String[] args) {
-		System.out.println("Primer reporsitio para git hab");
+		System.out.println("Primer repositorio para git hab");
 		System.out.println("Juan Carlos Molina");
 
 	}
